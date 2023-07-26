15e33ad45b59d53aff20ffd6db1a44e70e2afc5e
Iklima Mardiana <81839323+Iklimardiana@users.noreply.github.com>
2023-07-26 13:33:10 +0700

    modify detailcomment

--- src/components/detailThread/ThreadDetailCommentForm.jsx
+++ src/components/detailThread/ThreadDetailCommentForm.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CommentInput from '../comment/CommentInput';
-// import CommentList from '../comment/CommentList';
 import { detailProp } from '../../utils/propHelper';
 
 export default function ThreadDetailCommentForm({ detail }) {
-  // const { comments } = detail;
   return (
     <div className="space-y-4 mt-4">
       <div>
         <h3 className="text-lg text-white font-semibold">Add Comment</h3>
         <CommentInput threadId={detail.id} />
       </div>
-      {/* <div>
-        <h3 className="text-lg text-white font-semibold">{`Comments (${comments.length})`}</h3>
-        <CommentList comments={comments} />
-      </div> */}
     </div>
   );
 }
